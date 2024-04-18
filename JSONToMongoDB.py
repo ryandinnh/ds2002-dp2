6d4c794d949690970cd5cb34399850695d495f8d
Ryan Dinh <rqd3qmk@virginia.edu>
2024-04-18 22:07:57 +0000

    Added error handling and got it to connect to mongo (imported 228)

--- JSONToMongoDB.py
+++ JSONToMongoDB.py
@@ -13,8 +13,8 @@ def importJSONToMongoDB():\
 
     #connecting to mongoDB atlas
     MONGOPASS = os.getenv('MONGOPASS')
-    uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
-    client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+    #uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
+    client = MongoClient(MONGOPASS, connectTimeoutMS=200, retryWrites=True) #The original arguments from the instructions not needed because $MONGOPASS includes uri, user, and pass
     # specify a database
     db = client.rqd3qmk
     # specify a collection
@@ -24,22 +24,33 @@ def importJSONToMongoDB():\
     path = "data"
 
     for (root, dirs, file) in os.walk(path):
-        for f in file:
-            #importing
-            file_path = os.path.join(root, f)
-            with open(file_path, 'r') as file:
-                file_data = json.load(file)
-            # Inserting the loaded data in the collection
-            # if JSON contains data more than one entry
-            # insert_many is used else insert_one is used
-            if isinstance(file_data, list):
-                collection.insert_many(file_data)
-                totalImported += len(file_data)
-            else:
-                collection.insert_one(file_data)
-                totalImported += 1
+        try:
+            for f in file:
+                #importing
+                file_path = os.path.join(root, f)
+                with open(file_path, 'r') as file:
+                    file_data = json.load(file)
+                # Inserting the loaded data in the collection
+                # if JSON contains data more than one entry
+                # insert_many is used else insert_one is used
+                if isinstance(file_data, list):
+                    collection.insert_many(file_data)
+                    totalImported += len(file_data)
+                else:
+                    collection.insert_one(file_data)
+                    totalImported += 1
+        #error handling
+        #corrupted documents exist in the fileset
+        except json.JSONDecodeError:
+            totalCorrupted += 1
+        #complete documents could not be imported
+        except Exception as e:
+            print(f"Failed to import {filename}: {str(e)}")
+            totalOrphaned += 1
 
-    print(totalImported)
+    print(f"Total Imported: {totalImported}")
+    print(f"Total Orphaned: {totalOrphaned}")
+    print(f"Total Corrupted: {totalCorrupted}")
 
 
 #script execution
